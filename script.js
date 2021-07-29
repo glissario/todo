16b2985889224310a0f3d19fa520b684f3361a93
Christian Glissmann <cgliss2000@gmail.com>
2021-07-29 16:48:39 +0200

    refactor: e2e testing + github actions

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let actualFilter = null;
 let toDoList = [];
 let todos = [];
 let initTask = [];
-const filterEvent = document.querySelector("#radioOptions");
-const eventList = document.querySelector("#eventList");
+const filterEvent = document.querySelector('#radioOptions');
+const eventList = document.querySelector('#eventList');
 // class Todo -> construktor
 class Todo {
   constructor(desc, todoid, label) {
@@ -16,10 +16,10 @@ class Todo {
   }
 }
 // Eventlistener for addbutton opr input field (keyboard #13)
-let addButton = document.querySelector("#addButton");
-addButton.addEventListener("click", addListEntry);
-let addInput = document.querySelector("#taskInput");
-addInput.addEventListener("keydown", function (e) {
+let addButton = document.querySelector('#addButton');
+addButton.addEventListener('click', addListEntry);
+let addInput = document.querySelector('#taskInput');
+addInput.addEventListener('keydown', function (e) {
   if (e.keyCode === 13) {
     addListEntry(e);
   }
@@ -30,7 +30,7 @@ addInput.addEventListener("keydown", function (e) {
 // getting the right label color
 
 function labelColor() {
-  const labelColor = document.querySelectorAll(".label-color");
+  const labelColor = document.querySelectorAll('.label-color');
   for (let i = 0; i < 3; i++) {
     if (labelColor[i].checked) {
       color = labelColor[i].classList[1];
@@ -42,53 +42,53 @@ function labelColor() {
 // connect the new element with former list.
 
 function addListEntry(e) {
-  let input = document.querySelector("#taskInput").value;
+  let input = document.querySelector('#taskInput').value;
   if (input.length < 5) {
-    confirm("At least 5 characters pls");
+    confirm('At least 5 characters pls');
     return;
   }
-  const todoId = input.trim().toLowerCase().replaceAll(" ", "-");
+  const todoId = input.trim().toLowerCase().replaceAll(' ', '-');
   let labelBGcolor = labelColor();
   const newTodo = new Todo(input, todoId, labelBGcolor);
   todos.push(newTodo);
-  localStorage.setItem("Todo-storage", JSON.stringify(todos));
-  const newListElement = document.createElement("li");
+  localStorage.setItem('Todo-storage', JSON.stringify(todos));
+  const newListElement = document.createElement('li');
   newListElement.todoObj = newTodo;
 
   //newTodo.dom = newListElement;
   newListElement.classList =
-    labelBGcolor + " " + "yourToDo__eventList__listElement";
+    labelBGcolor + ' ' + 'yourToDo__eventList__listElement';
   //NewListElement.style.backgroundColor = labelBGcolor;
-  const ncheckbox = document.createElement("input");
-  ncheckbox.type = "checkbox";
-  ncheckbox.classList = "taskCheckbox";
-  ncheckbox.id = "taskCheckbox";
-  const ncurrentLabel = document.createElement("label");
-  ncurrentLabel.classList = "taskName";
+  const ncheckbox = document.createElement('input');
+  ncheckbox.type = 'checkbox';
+  ncheckbox.classList = 'taskCheckbox';
+  ncheckbox.id = 'taskCheckbox';
+  const ncurrentLabel = document.createElement('label');
+  ncurrentLabel.classList = 'taskName';
   //ncurrentLabel.setAttribute("for", todoId);
   const nnode = document.createTextNode(input);
-  const newColorButton = document.createElement("button");
-  const buttonNode = document.createTextNode("new Color");
+  const newColorButton = document.createElement('button');
+  const buttonNode = document.createTextNode('new Color');
   newColorButton.appendChild(buttonNode);
-  newColorButton.classList = "button";
+  newColorButton.classList = 'button';
   ncurrentLabel.appendChild(nnode);
-  const oldList = document.querySelector("#eventList");
+  const oldList = document.querySelector('#eventList');
   oldList.appendChild(newListElement);
   sort();
-  input = document.querySelector("#taskInput").value = "";
+  input = document.querySelector('#taskInput').value = '';
 }
-eventList.addEventListener("change", function (e) {
+eventList.addEventListener('change', function (e) {
   const newDoneState = e.target.checked;
   const todoObj = e.target.parentElement.todoObj;
   todoObj.status = newDoneState;
-  localStorage.setItem("Todo-storage", JSON.stringify(todos));
+  localStorage.setItem('Todo-storage', JSON.stringify(todos));
 });
 // removeButton and function
 // refactored
-let removeButton = document.querySelector("#removeButton");
-removeButton.addEventListener("click", removeDoneTasks);
+let removeButton = document.querySelector('#removeButton');
+removeButton.addEventListener('click', removeDoneTasks);
 function removeDoneTasks() {
-  const taskList = document.querySelector(".yourToDo__eventList");
+  const taskList = document.querySelector('.yourToDo__eventList');
   for (let i = taskList.children.length - 1; i >= 0; i--) {
     let cb = taskList.children[i].todoObj.status;
     if (cb) {
@@ -97,12 +97,12 @@ function removeDoneTasks() {
       taskList.children[i].remove();
     }
   }
-  localStorage.setItem("Todo-storage", JSON.stringify(todos));
+  localStorage.setItem('Todo-storage', JSON.stringify(todos));
 }
 // Filter List
 // get to know the active Filter
 function actualToFilter() {
-  filterField = document.querySelectorAll("#filter");
+  filterField = document.querySelectorAll('#filter');
   for (let i = 0; i < 3; i++) {
     if (filterField[i].checked) {
       actualFilter = filterField[i].value;
@@ -113,42 +113,42 @@ function actualToFilter() {
 // case distinction of values parent-radiobutton
 // show all/open/done
 // not refactured yet
-filterEvent.addEventListener("change", function (e) {
+filterEvent.addEventListener('change', function (e) {
   actualToFilter();
   switch (actualFilter) {
-    case "all": // all Tasks
+    case 'all': // all Tasks
       const allTask = document.querySelectorAll(
-        ".yourToDo__eventList__listElement"
+        '.yourToDo__eventList__listElement'
       );
       for (let i = 0; i < allTask.length; i++) {
-        allTask[i].style.display = "";
+        allTask[i].style.display = '';
       }
       break;
-    case "open": // show the open, hide the done
+    case 'open': // show the open, hide the done
       const openTask = document.querySelectorAll(
-        ".yourToDo__eventList__listElement"
+        '.yourToDo__eventList__listElement'
       );
       for (let i = 0; i < openTask.length; i++) {
         let cb = openTask[i].firstChild;
         if (cb.checked) {
-          openTask[i].style.display = "none";
+          openTask[i].style.display = 'none';
         }
         if (!cb.checked) {
-          openTask[i].style.display = "";
+          openTask[i].style.display = '';
         }
       }
       break;
-    case "done": // show the done, hide the open
+    case 'done': // show the done, hide the open
       const doneTask = document.querySelectorAll(
-        ".yourToDo__eventList__listElement"
+        '.yourToDo__eventList__listElement'
       );
       for (let i = 0; i < doneTask.length; i++) {
         let cb = doneTask[i].firstChild;
         if (!cb.checked) {
-          doneTask[i].style.display = "none";
+          doneTask[i].style.display = 'none';
         }
         if (cb.checked) {
-          doneTask[i].style.display = "";
+          doneTask[i].style.display = '';
         }
       }
       break;
@@ -156,26 +156,26 @@ filterEvent.addEventListener("change", function (e) {
 });
 function sort() {
   let todoList = [];
-  todoList = document.querySelectorAll(".yourToDo__eventList__listElement");
-  const oldList = document.querySelector("#eventList");
+  todoList = document.querySelectorAll('.yourToDo__eventList__listElement');
+  const oldList = document.querySelector('#eventList');
   for (let i = oldList.children.length - 1; i >= 0; i--) {
     oldList.children[i].remove();
   }
   todos.forEach(function (todo) {
-    if (todo.labelClass === "bg-color1") {
+    if (todo.labelClass === 'bg-color1') {
       newElement = addElementListFromObj(todo);
 
       oldList.appendChild(newElement);
     }
   });
   todos.forEach(function (todo) {
-    if (todo.labelClass === "bg-color2") {
+    if (todo.labelClass === 'bg-color2') {
       newElement = addElementListFromObj(todo);
       oldList.appendChild(newElement);
     }
   });
   todos.forEach(function (todo) {
-    if (todo.labelClass === "bg-color3") {
+    if (todo.labelClass === 'bg-color3') {
       newElement = addElementListFromObj(todo);
       oldList.appendChild(newElement);
     }
@@ -187,49 +187,49 @@ function sort() {
 // save Array "todos"
 initApp();
 function initApp() {
-  if (localStorage.getItem("Todo-storage") !== null) {
-    initTask = JSON.parse(localStorage.getItem("Todo-storage"));
+  if (localStorage.getItem('Todo-storage') !== null) {
+    initTask = JSON.parse(localStorage.getItem('Todo-storage'));
     todos = initTask;
     for (i = 0; i < initTask.length; i++) {
-      const line = document.createElement("li");
+      const line = document.createElement('li');
       line.todoObj = initTask[i];
-      const checkbox = document.createElement("input");
-      const currentLabel = document.createElement("label");
+      const checkbox = document.createElement('input');
+      const currentLabel = document.createElement('label');
       const node = document.createTextNode(initTask[i].description);
-      checkbox.type = "checkbox";
-      checkbox.classList = "taskCheckbox";
-      checkbox.id = "taskCheckbox";
+      checkbox.type = 'checkbox';
+      checkbox.classList = 'taskCheckbox';
+      checkbox.id = 'taskCheckbox';
       checkbox.checked = initTask[i].status;
       //currentLabel.for = "taskCheckbox";
-      currentLabel.classList = "taskName";
+      currentLabel.classList = 'taskName';
       //line.classList = "yourToDo__eventList__listElement";
       line.classList =
-        initTask[i].labelClass + " " + "yourToDo__eventList__listElement";
+        initTask[i].labelClass + ' ' + 'yourToDo__eventList__listElement';
       line.appendChild(checkbox);
       line.appendChild(currentLabel);
       currentLabel.appendChild(node);
-      const oldList = document.querySelector("#eventList");
+      const oldList = document.querySelector('#eventList');
       oldList.appendChild(line);
       sort();
     }
   }
 }
 function addElementListFromObj(element) {
-  const line = document.createElement("li");
+  const line = document.createElement('li');
   line.todoObj = element;
-  const checkbox = document.createElement("input");
-  const currentLabel = document.createElement("label");
+  const checkbox = document.createElement('input');
+  const currentLabel = document.createElement('label');
   const node = document.createTextNode(element.description);
-  checkbox.type = "checkbox";
-  checkbox.classList = "taskCheckbox";
-  checkbox.id = "taskCheckbox";
+  checkbox.type = 'checkbox';
+  checkbox.classList = 'taskCheckbox';
+  checkbox.id = 'taskCheckbox';
   checkbox.checked = element.status;
   line.appendChild(checkbox);
 
-  currentLabel.classList = "taskName";
+  currentLabel.classList = 'taskName';
   line.classList = element.labelClass;
   line.classList =
-    element.labelClass + " " + "yourToDo__eventList__listElement";
+    element.labelClass + ' ' + 'yourToDo__eventList__listElement';
 
   line.appendChild(currentLabel);
   currentLabel.appendChild(node);
---
+++ src/script.js
@@ -0,0 +1,370 @@
+// global Variables:
+let filterField = null;
+let actualFilter = null;
+// const toDoList = []
+let todos = [];
+// const initTask = []
+
+const filterEvent = document.querySelector('#radioOptions');
+const eventList = document.querySelector('#eventList');
+
+// class Todo -> construktor
+
+class Todo {
+  constructor(desc) {
+    this.description = desc;
+    this.done = false;
+  }
+}
+
+// Eventlistener for addbutton opr input field (keyboard #13)
+
+const addButton = document.querySelector('#addButton');
+addButton.addEventListener('click', addListEntry);
+
+const addInput = document.querySelector('#taskInput');
+addInput.addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
+    addListEntry(e);
+  }
+});
+
+// getting the right label color
+
+/* function labelColor() {
+  const labelColor = document.querySelectorAll(".label-color");
+  for (let i = 0; i < 3; i++) {
+    if (labelColor[i].checked) {
+      color = labelColor[i].classList[1];
+      return color;
+    }
+  }
+} */
+
+function createRedColorButton() {
+  const changeColorButton1 = document.createElement('button');
+  changeColorButton1.classList = 'color-button1';
+  return changeColorButton1;
+}
+
+function createOrangeColorButton() {
+  const changeColorButton1 = document.createElement('button');
+  changeColorButton1.classList = 'color-button2';
+  return changeColorButton1;
+}
+
+function createGreenColorButton() {
+  const changeColorButton1 = document.createElement('button');
+  changeColorButton1.classList = 'color-button3';
+  return changeColorButton1;
+}
+
+function addListEntry() {
+  let input = document.querySelector('#taskInput').value;
+
+  if (input.length < 5) {
+    confirm('At least 5 characters pls');
+    return;
+  }
+
+  const newTodo = new Todo(input);
+
+  todos.push(newTodo);
+
+  localStorage.setItem('Todo-storage', JSON.stringify(todos));
+
+  const newListElement = document.createElement('li');
+  newListElement.todoObj = newTodo;
+
+  addElementListFromObj(newTodo);
+
+  PostRestData(newTodo);
+  getRestID();
+
+  input = document.querySelector('#taskInput').value = '';
+}
+
+// e.target größer setzen?!
+eventList.addEventListener('change', function (e) {
+  const newDoneState = e.target.checked;
+  const todoObj = e.target.parentElement.todoObj;
+  todoObj.done = newDoneState;
+  PutRestData(todoObj);
+  localStorage.setItem('Todo-storage', JSON.stringify(todos));
+});
+
+// removeButton and function
+// refactored
+
+const removeButton = document.querySelector('#removeButton');
+removeButton.addEventListener('click', removeDoneTasks);
+
+function removeDoneTasks() {
+  const taskList = document.querySelector('.yourToDo__eventList');
+  for (let i = taskList.children.length - 1; i >= 0; i--) {
+    const cb = taskList.children[i].todoObj.done;
+    if (cb) {
+      deleteRestData(taskList.children[i].todoObj);
+
+      const index = todos.indexOf(taskList.children[i].todoObj);
+      todos.splice(index, 1);
+      taskList.children[i].remove();
+    }
+  }
+  localStorage.setItem('Todo-storage', JSON.stringify(todos));
+}
+
+// Filter List
+
+// get to know the active Filter
+function actualToFilter() {
+  filterField = document.querySelectorAll('#filter');
+  for (let i = 0; i < 3; i++) {
+    if (filterField[i].checked) {
+      actualFilter = filterField[i].value;
+    }
+  }
+}
+// -> run the filter:
+// case distinction of values parent-radiobutton
+// show all/open/done
+
+// not refactured yet
+filterEvent.addEventListener('change', function () {
+  actualToFilter();
+  const allTask = document.querySelectorAll(
+    '.yourToDo__eventList__listElement'
+  );
+  const openTask = document.querySelectorAll(
+    '.yourToDo__eventList__listElement'
+  );
+  const doneTask = document.querySelectorAll(
+    '.yourToDo__eventList__listElement'
+  );
+
+  switch (actualFilter) {
+    case 'all': // all Tasks
+      for (let i = 0; i < allTask.length; i++) {
+        allTask[i].style.display = '';
+      }
+      break;
+
+    case 'open': // show the open, hide the done
+      for (let i = 0; i < openTask.length; i++) {
+        const cb = openTask[i].firstChild;
+        if (cb.checked) {
+          openTask[i].style.display = 'none';
+        }
+        if (!cb.checked) {
+          openTask[i].style.display = '';
+        }
+      }
+      break;
+
+    case 'done': // show the done, hide the open
+      for (let i = 0; i < doneTask.length; i++) {
+        const cb = doneTask[i].firstChild;
+        if (!cb.checked) {
+          doneTask[i].style.display = 'none';
+        }
+        if (cb.checked) {
+          doneTask[i].style.display = '';
+        }
+      }
+      break;
+  }
+});
+
+/* function sort() {
+  let todoList = [];
+  todoList = document.querySelectorAll(".yourToDo__eventList__listElement");
+  const oldList = document.querySelector("#eventList");
+
+  for (let i = oldList.children.length - 1; i >= 0; i--) {
+    oldList.children[i].remove();
+  }
+
+  todos.forEach(function (todo) {
+    if (todo.labelClass === "bg-color1") {
+      newElement = addElementListFromObj(todo);
+      oldList.appendChild(newElement);
+    }
+  });
+
+  todos.forEach(function (todo) {
+    if (todo.labelClass === "bg-color2") {
+      newElement = addElementListFromObj(todo);
+
+      oldList.appendChild(newElement);
+    }
+  });
+
+  todos.forEach(function (todo) {
+    if (todo.labelClass === "bg-color3") {
+      newElement = addElementListFromObj(todo);
+
+      oldList.appendChild(newElement);
+    }
+  });
+} */
+
+// Local storage: init App after refresh
+// initTask becomes Array with Tasks as an object from local storage
+// initializing the Tasks
+// save Array "todos" -- commented out cause of using REST-data
+/*
+initApp();
+function initApp() {
+  if (localStorage.getItem("Todo-storage") !== null) {
+    initTask = JSON.parse(localStorage.getItem("Todo-storage"));
+    todos = initTask;
+    for (i = 0; i < initTask.length; i++) {
+      addElementListFromObj(initTask[i]);
+      const line = document.createElement("li");
+      line.todoObj = initTask[i];
+      addElementListFromObj(initTask[i]);
+      const oldList = document.querySelector("#eventList");
+      oldList.appendChild(line);
+      sort();
+    }
+  }
+}
+*/
+// get an element, type todoObl -> return a Node to add it to the DOM.
+
+function addElementListFromObj(element) {
+  const line = document.createElement('li');
+  line.todoObj = element;
+
+  const checkbox = document.createElement('input');
+  const currentLabel = document.createElement('label');
+  const node = document.createTextNode(element.description);
+
+  checkbox.type = 'checkbox';
+  checkbox.classList = 'taskCheckbox';
+  checkbox.id = 'taskCheckbox';
+  checkbox.checked = element.done;
+  checkbox.setAttribute('data-test', 'cb-remove');
+  line.appendChild(checkbox);
+
+  currentLabel.classList = 'taskName';
+  line.classList =
+    element.labelClass + ' ' + 'yourToDo__eventList__listElement';
+  currentLabel.setAttribute('data-test', 'task');
+
+  const colorButton1 = createRedColorButton();
+  const colorButton2 = createOrangeColorButton();
+  const colorButton3 = createGreenColorButton();
+
+  line.appendChild(currentLabel);
+  currentLabel.appendChild(node);
+  line.appendChild(colorButton1);
+  line.appendChild(colorButton2);
+  line.appendChild(colorButton3);
+
+  const oldList = document.querySelector('#eventList');
+  oldList.appendChild(line);
+}
+
+function getRestID() {
+  fetch('http://localhost:4730/todos')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const lastElement = document.querySelector(
+        '.yourToDo__eventList'
+      ).lastChild;
+
+      data.forEach(function (item) {
+        if (item.description.includes(lastElement.todoObj.description)) {
+          lastElement.todoObj.id = item.id;
+        }
+      });
+    });
+}
+GetRestData();
+
+function GetRestData() {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  // var raw = JSON.stringify({
+  //  description: "Learn PA",
+  //  done: false,
+  // });
+
+  const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  fetch('http://localhost:4730/todos/', requestOptions)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      todos = data;
+
+      for (let i = 0; i < data.length; i++) {
+        const newListElement = document.createElement('li');
+        newListElement.todoObj = addElementListFromObj(data[i]);
+      }
+    });
+}
+
+function PostRestData(element) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify(element),
+    redirect: 'follow'
+  };
+
+  fetch('http://localhost:4730/todos/', requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
+}
+
+function deleteRestData(element) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    body: JSON.stringify(element),
+    redirect: 'follow'
+  };
+
+  const delLink = 'http://localhost:4730/todos/' + element.id;
+
+  fetch(delLink, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
+}
+
+function PutRestData(element) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: JSON.stringify(element),
+    redirect: 'follow'
+  };
+
+  const changeLink = 'http://localhost:4730/todos/' + element.id;
+
+  fetch(changeLink, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
+}
